bd98097acdac4412998a98f46dd5c8f4e08dbd20
vgrgala@gmail.com <Chinna@1025>
2023-06-22 14:51:51 -0500

    Added req code for create-employee component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
@@ -16,7 +16,8 @@ import { CreateEmployeeComponent } from './create-employee/create-employee.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/create-employee/create-employee.component.ts
+++ src/app/create-employee/create-employee.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-employee',
   templateUrl: './create-employee.component.html',
   styleUrls: ['./create-employee.component.css']
 })
-export class CreateEmployeeComponent {
+export class CreateEmployeeComponent implements OnInit {
+
+  employee: Employee = new Employee();
+
+  constructor(private employeeService: EmployeeService,
+    private router:Router){
+
+  }
+  ngOnInit() {
+    
+  }
+
+  createEmployee(){
+    this.employeeService.createEmployee(this.employee).subscribe({
+      next: (data) => {
+      console.log(data);
+      this.goToEmployeeList();
+    }, 
+    error: (error) => console.log(error)
+  });
+  }
+
+  goToEmployeeList(){
+       this.router.navigate(['/employees']);
+  }
+
+  onSubmit(){
+    console.log(this.employee);
+    this.createEmployee();
+  }
 
 }
