78a2f770de2d853fd70fd7b830562ec232d0a4ea
vgrgala@gmail.com <Chinna@1025>
2023-06-23 16:14:16 -0500

    Added form for update-employee component

--- src/app/update-employee/update-employee.component.ts
+++ src/app/update-employee/update-employee.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
 
 @Component({
   selector: 'app-update-employee',
   templateUrl: './update-employee.component.html',
   styleUrls: ['./update-employee.component.css']
 })
-export class UpdateEmployeeComponent {
+export class UpdateEmployeeComponent implements OnInit{
 
+  employee!: Employee;
+  constructor(){
+
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  onSubmit(){
+    
+  }
 }
